7cc79375468f6773267845e252dbb8ab59e41c51
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2026-02-12 12:13:22 +0100

    Update textwipe.tsx
    Fixed import statement to fit latest motion package

--- src/components/textwipe.tsx
+++ src/components/textwipe.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion, AnimatePresence } from 'motion/react'
 
 const words = [
   'Your Data...',
